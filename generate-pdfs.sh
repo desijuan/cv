6c50bfb59210c2d395e63c645d57f5e1fda939f2
Juan Desimoni <desijuan89@gmail.com>
2022-09-16 14:07:35 -0300

    mejoré generate-pdfs

--- generate-pdfs.sh
+++ generate-pdfs.sh
@@ -3,11 +3,13 @@
 date=$(date +%b-%Y)
 commitHash=$(git rev-parse --short HEAD)
 
+git restore cv-{en,es,pt}.tex
+
 rm *.pdf
 
 latexmk -pdf
 
-for file in *.pdf
+for file in cv-{en,es,pt}.pdf
 do
     mv $file desimoni-${file%.*}-$date-$commitHash.pdf
 done
