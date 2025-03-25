0ac2e1cf98d443c53209b28c7df0bd2c445d4e0f
Juan Desimoni <juan.desimoni@codex.com.br>
2025-03-25 20:51:19 -0300

    Update: keep only the english version

--- .generate-pdfs.sh
+++ .generate-pdfs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if git restore cv-{en,es,pt}.tex; then
+if git restore cv.tex; then
   date=$(date +%b-%Y)
   commitHash=$(git rev-parse --short HEAD)
   rm *.pdf
   latexmk -pdf
-  for file in cv-{en,es,pt}.pdf; do
+  for file in cv.pdf; do
       mv $file desimoni-${file%.*}-$date-$commitHash.pdf
   done
   latexmk -c
 fi
-
